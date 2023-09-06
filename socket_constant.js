5f2fd94b2cb66157c4effe135ffc04faee09a203
Dushyant Sharma <54388114+dushyantsharma891@users.noreply.github.com>
2023-09-06 14:35:15 +0530

    Add files via upload

---
+++ socket_constant.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C)  - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential. Dissemination of this information or reproduction 
+ * of this material is strictly forbidden unless prior written permission is obtained
+ * from .
+ * 
+ * Written By  : 
+ * Description :
+ * Modified By :
+ */	
+
+const  constantObj = {
+
+    LISTENER : {
+        MAIN_LISTENER : 'call',
+    },
+    
+    EMIT : {
+        MAIN_EMIT : 'call',
+        MAIN_EMIT_LIVE : 'live'
+
+    },
+   
+    
+    CALL_TYPES : {
+        INCOMING_CALL_INDICATION    : 'INSTANT-CALL',
+        END_CALL_INDICATION         : 'END-CALL',
+        ACCEPT_CALL_INDICATION      : 'ACCEPT',
+        DISCONNECT_CALL_INDICATION  : 'DISCONNECT',
+        REJECT_CALL_INDICATION      : 'REJECT'
+    },
+
+    ACTION_TYPES : {
+        INCOMING_CALL               : 'INCOMING-CALL',
+        QUESTIONS_LIST              : 'QUESTIONS-LIST',
+        QUESTIONS_ADD_NEW           : '',
+        QUESTIONS_MARK_DONE         : '',
+        ACCEPT_CALL                 : 'ACCEPT',
+        DISCONNECT_CALL             : 'DISCONNECT',
+        REJECT_CALL                 : 'REJECT',
+        END_CALL                    : 'END',
+        CANDIDATE                   : 'CANDIDATE'
+
+    },
+
+    FCM_ACTION : {
+        INCOMING_CALL               : 'Incoming Call',
+
+    },
+    FCM_MESSAGE : {
+        INCOMING_CALL               : 'Hi you have an incoming call.'
+    }
+};
+module.exports = constantObj;
